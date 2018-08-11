a70c7ca0997aa73ae1ca64910fbec4342eacb147
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-11 21:10:26 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,9 +9,9 @@ class SessionsController < ApplicationController
       redirect_to '/login'
     else
       session[:name] = params[:name]
+      redirect_to '/wecome'
     end
   end 
-  end
 
   def welcome
     if logged_in
