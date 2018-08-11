dd15d466af11f4531efb03a919a21b50546c307b
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-11 21:04:30 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  root controller: 'sessions', action: 'new'
+  root 'secrets#welcome'
   get '/login'=> 'sessions#new'
   post '/login' => 'sessions#create'
 
