66ccb166d5e4ec67e3f14136c8ad75363edb4548
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-11 20:56:17 +0000

    automatically backed up by learn

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,23 @@
+class SecretsController < ApplicationController
+  before_action :logged_in
+    def show
+      if !logged_in
+        redirect_to controller: 'sessions', action:'new'
+      end
+
+    end
+
+
+
+    private
+
+    def logged_in
+      if !params[:name] || params[:name] == ""
+        redirect_to controller: 'sessions', action:'new'
+      else
+      session[:name] = params[:name]
+      redirect_to welcome page
+    end
+  end 
+
+end
