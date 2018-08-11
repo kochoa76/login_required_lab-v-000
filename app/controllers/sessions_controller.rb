b743837b9e0d8831f19d38619e14bab69cdc614c
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-11 22:08:45 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,10 +26,9 @@ class SessionsController < ApplicationController
  end
 
   private
-  def logged_in
-      if !params[:name] || params[:name] == ""
-
-        redirect_to controller: 'sessions', action: 'new'
+    def logged_in
+      if params[:name] == ""
+      redirect_to controller: 'sessions', action: 'new'
       else
       session[:name] = params[:name]
     end
