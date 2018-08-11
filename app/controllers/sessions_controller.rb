3352558dbfd54227d3d1521c241e557302a9e63e
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-11 21:50:49 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     redirect_to(controller: 'sessions', action: 'new')
     else
       session[:name] = params[:name]
-      redirect_to controller: 'secrets' action: 'show'
+    redirect_to controller: 'sessions', action: 'welcome'
     end
   end
 
