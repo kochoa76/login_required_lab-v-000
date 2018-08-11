d7d05134ddbfaa6807144d07c90801734ce872b1
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-11 21:08:15 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
 
 
+  def new
+  end
+
+  def create
+  end
+
+  def welcome
+    if logged_in
+      redirect_to controller: 'secrets', action: 'show'
+    else
+      redirect_to controller: 'sessions', action: 'new'
+    end
+  end 
 end
