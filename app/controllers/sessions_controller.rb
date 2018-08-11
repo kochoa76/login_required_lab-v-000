0a9728517e0e64bbeb1dca6ae0aa53499cc92af7
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-11 21:09:35 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if !params[:name] || params[:name]==""
+      redirect_to '/login'
+    else 
+      session[:name] = params[:name]
+    end 
+  end 
   end
 
   def welcome
