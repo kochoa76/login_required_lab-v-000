c2dfa6f804c90791341f6a4bd2fa44fa6d8a9372
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-11 22:01:47 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
   def create
     raise params.inspect
-    if !params[:name] || params[:name].empty?
+    if params[:name].empty?
     redirect_to(controller: 'sessions', action: 'new')
     else
       session[:name] = params[:name]
