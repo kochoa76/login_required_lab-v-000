8f5cd583735e7022767b72bcf89fe9b5e3af8f32
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-11 21:45:14 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
 
   def welcome
     if !logged_in
-      redirect_to '/login'
+      redirect_to controller: 'sessions', action: 'new'
     end
   end
 
